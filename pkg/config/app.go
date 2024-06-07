49c104cc83bc2765b49d1222220ed9399b0ec226
Manav <101001298+imanav10@users.noreply.github.com>
2024-06-07 08:34:33 +0530

    chore: Update import statement in app.go for consistency

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,10 +1,10 @@
 package config 
 
 
-import {
+import(
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-}
+)
 
 
 var (
@@ -14,11 +14,11 @@ var (
 func Connect(){
 	d, err := gorm.Open("mysql", "root:root/simplerest?charset-utf8&parseTime=True&loc=Local")
 	if err!= nil {
-		panic("Failed to connect to database! %s", err)
+		panic(err)
 	}
 	db = d
 }
 
-fucn GetDB() *gorm.db(
+func GetDB() *gorm.DB{
 	return db
-)
+}
